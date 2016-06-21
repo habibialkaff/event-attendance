79ab6498ff3a51d2def3fa4eed6078af3e1ed0d6
Habibi <guilvee@gmail.com>
2016-06-22 06:40:40 +0800

    Fix package.json firebase deployment command

--- devServer.js
+++ devServer.js
@@ -7,28 +7,28 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-    stats: {
-        colors: true,
-        hash: false,
-        timings: true,
-        chunks: false,
-        chunkModules: false,
-        modules: false
-    },
-    publicPath: config.output.publicPath
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  },
+  publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, 'localhost', function(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+app.listen(8080, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log('Listening at http://localhost:8080');
+  console.log('Listening at http://localhost:8080');
 });
